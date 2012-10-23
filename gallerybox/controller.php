67f84ce708ffc45f03f625537147245f35d42a63
Jeramy <jedspurg@iupui.edu>
2012-10-23 08:33:29 -0400

    Recent Images block bug fixes, v 1.6.6.1

--- gallerybox/controller.php
+++ gallerybox/controller.php
@@ -6,7 +6,7 @@ class GalleryboxPackage extends Package {
 
 	protected $pkgHandle = 'gallerybox';
 	protected $appVersionRequired = '5.5';
-	protected $pkgVersion = '1.6.6';
+	protected $pkgVersion = '1.6.6.1';
 	
 	public function getPackageDescription() {
 		return t('Adds a user gallery to your website.');
